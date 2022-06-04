a440a6241192fa6d881dddb8ae2609696715e6f6
mcubek <matej.cubek@neos.hr>
2022-06-04 20:37:04 +0200

    Zad3 done

--- lab3/models.py
+++ lab3/models.py
@@ -22,27 +22,49 @@ class BaselineModel(nn.Module):
         return h.flatten()
 
 
-class LstmRnnModel(nn.Module):
+class RnnModel(nn.Module):
+    def lstm_forward(self, x):
+        _, (h, _) = self.rnn(x)
+
+        return h[-1]
+
+    def plain_rnn_forward(self, x):
+        _, h = self.rnn(x)
+
+        return h[-1]
+
     def __init__(self, embedding_matrix, args):
         super().__init__()
         self.embedding_matrix = embedding_matrix
 
-        self.lstm = nn.LSTM(input_size=args.embedding_size,
-                            hidden_size=args.rnn_hidden_size,
-                            num_layers=args.rnn_num_layers,
-                            dropout=args.rnn_dropout,
-                            bidirectional=args.rnn_bidirectional)
+        rnn_params = {
+            "input_size": args.embedding_size,
+            "hidden_size": args.rnn_hidden_size,
+            "num_layers": args.rnn_num_layers,
+            "dropout": args.rnn_dropout,
+            "bidirectional": args.rnn_bidirectional
+        }
+
+        if args.rnn_type == "lstm":
+            self.rnn = nn.LSTM(**rnn_params)
+            self.rnn_forward = self.lstm_forward
+        elif args.rnn_type == "plain":
+            self.rnn = nn.RNN(**rnn_params)
+            self.rnn_forward = self.plain_rnn_forward
+        elif args.rnn_type == "gru":
+            self.rnn = nn.GRU(**rnn_params)
+            self.rnn_forward = self.plain_rnn_forward
+        else:
+            raise AttributeError
 
         self.fc_1 = nn.Linear(args.rnn_hidden_size, args.rnn_hidden_size)
         self.fc_2 = nn.Linear(args.rnn_hidden_size, 1)
 
     def forward(self, x, length):
-        h = self.embedding_matrix(x)
-
-        _, (h_lstm, _) = self.lstm(h)
-        h = h_lstm[-1].reshape((h_lstm.shape[1], h_lstm.shape[2]))
-
+        batch = self.embedding_matrix(x)
+        batch_t = torch.transpose(batch, 0, 1)
+        h = self.rnn_forward(batch_t)
         h = self.fc_1(h)
-        h = torch.relu(h)
-        h = self.fc_2(h)
+        s = torch.relu(h)
+        h = self.fc_2(s)
         return h.flatten()
--- lab3/zad_3_rnn.py
+++ lab3/zad_3_rnn.py
@@ -5,7 +5,7 @@ import torch
 from torch import nn
 
 from lab3.lab_utils import load_dataset, VECTOR_PATH, evaluate, load_data_loaders, train
-from lab3.models import BaselineModel, LstmRnnModel
+from lab3.models import BaselineModel, RnnModel
 
 
 @dataclass
@@ -14,7 +14,7 @@ class Args:
     train_batch_size: int
     valid_batch_size: int
     test_batch_size: int
-    seed: int
+    seed: list[int]
     epochs: int
     lr: float
     max_norm: float
@@ -22,43 +22,72 @@ class Args:
     rnn_num_layers: int
     rnn_dropout: float
     rnn_bidirectional: bool
+    rnn_type: str
 
 
 args = Args(300,
             10,
             32,
             32,
-            2022,
-            100,
-            1e-5,
+            [2022, 1999, 2023, 9999, 1111],
+            10,
+            5e-5,
             0.25,
             150,
             2,
             0,
-            False)
+            False,
+            "lstm")
+
+
+def save_results_to_file(results: dict, file_path):
+    result_string = ""
+    for seed, result in results.items():
+        result_string += f"""
+Seed = {seed}
+Accuracy = {result["accuracy"]:.2f}
+F1 Score = {result["f1_score"]:.2f}
+Avg Loss = {result["avg_loss"]:.2f}
+Confusion Matrix =
+{result["confusion_matrix"]}
+        """
+
+    with open(file_path, "w") as file:
+        file.write(result_string)
+        file.flush()
+
 
 if __name__ == '__main__':
-    np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
 
-    train_dataset, valid_dataset, test_dataset = load_dataset(vocab_min_freq=1, vocab_max_size=-1)
-    train_dataloader, valid_dataloader, test_dataloader = load_data_loaders(train_dataset,
-                                                                            valid_dataset,
-                                                                            test_dataset,
-                                                                            args)
+    results = {}
 
-    embedding_matrix = train_dataset.get_embedding_matrix(VECTOR_PATH)
+    for seed in args.seed:
+        np.random.seed(seed)
+        torch.manual_seed(seed)
 
-    model = LstmRnnModel(embedding_matrix, args)
+        print(f'\nseed={seed}')
 
-    criterion = nn.BCEWithLogitsLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+        train_dataset, valid_dataset, test_dataset = load_dataset(vocab_min_freq=1, vocab_max_size=-1)
+        train_dataloader, valid_dataloader, test_dataloader = load_data_loaders(train_dataset,
+                                                                                valid_dataset,
+                                                                                test_dataset,
+                                                                                args)
 
-    for epoch in range(args.epochs):
-        print(f'Epoch {epoch + 1}.')
-        train(model, train_dataloader, optimizer, criterion, max_norm=args.max_norm)
-        print(f'Evaluating on validation dataset:')
-        evaluate(model, valid_dataloader, criterion)
+        embedding_matrix = train_dataset.get_embedding_matrix(VECTOR_PATH)
 
-    print(f'Evaluating on test dataset:')
-    evaluate(model, test_dataloader, criterion)
+        model = RnnModel(embedding_matrix, args)
+
+        criterion = nn.BCEWithLogitsLoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+
+        for epoch in range(args.epochs):
+            print(f'Epoch {epoch + 1}.')
+            train(model, train_dataloader, optimizer, criterion, max_norm=args.max_norm, print_progress=False)
+            print(f'Evaluating on validation dataset:')
+            evaluate(model, valid_dataloader, criterion)
+
+        print(f'Evaluating on test dataset:')
+
+        results[seed] = evaluate(model, test_dataloader, criterion)
+
+    save_results_to_file(results, "./zad3.txt")
